ecc7c2733df125d2da7ecddac01c9226be038624
André <67884305+andrcxx@users.noreply.github.com>
2020-11-21 02:29:18 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	
+	float a;	
+	
+	cout<<"Digite o seu numero:"<<endl; 
+	cin>>a;
+	
+	if(a<5){
+		
+		cout<<"O numero e igual ou menor que 5."<<endl;
+	}	
+	else{
+		
+		cout<<"O numero e maior que 5."<<endl;
+			
+	}
+	system("pause");
+	return 0;
+}
